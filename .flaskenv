b489d677e1c705e9fe7a83c8b38e535d69159916
Nainu Wadhwa <Nainu@UGC-B-LAP-0012.local>
2022-05-16 13:06:48 +0530

    front end added

---
+++ .flaskenv
@@ -0,0 +1,3 @@
+export FLASK_APP=app.py
+export FLASK_ENV=development
+export JWT_SECRET_KEY='jwt_secret_key'
--- main.py
+++ main.py
@@ -1,5 +1,32 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,Blueprint,app, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from sqlalchemy import *
+import os
+from flask_migrate import Migrate
+from flask_jwt_extended import jwt_required, create_access_token, create_refresh_token, get_jwt_identity
+from constants.http_status_codes import HTTP_200_OK,HTTP_401_UNAUTHORIZED
+from flask_jwt_extended import JWTManager
+JWT_SECRET_KEY=os.environ.get('jwt_secret_key')
 app = Flask(__name__)
+jwt=JWTManager(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:UPGRAD@localhost/placement'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "random string"
+
+db = SQLAlchemy(app)
+migrate=Migrate(app,db)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    # is_superuser=db.Column(Boolean, unique=False,default=False)
+    # is_active=db.Column(Boolean, unique=False,default=True)
+
+    def __repr__(self):
+        return f'<User: {self.email}>'
+
+db.create_all()
 
 @app.route("/")
 def index():
@@ -10,9 +37,9 @@ def index():
 def drive():
     return render_template('drives.html')
 
-@app.route("/login")
-def login():
-    return render_template('login.html')
+# @app.route("/login")
+# def login():
+#     return render_template('login.html')
 
 @app.route("/add")
 def adddrive():
@@ -30,6 +57,30 @@ def learners():
 def addlearner():
     return render_template('addlearner.html')
 
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
 @app.route("/applicants")
 def applicants():
     return render_template('applicants.html')
+
+@app.post('/api/login')
+def userlogin():
+    email = request.json.get('email', '')
+    user = User.query.filter_by(email=email).first()
+
+    if user:
+        refresh = create_refresh_token(identity=user.id)
+        access = create_access_token(identity=user.id)
+
+        return jsonify({
+                'user': {
+                    'refresh': refresh,
+                    'access': access,
+                    'email': user.email
+                }
+
+            }), HTTP_200_OK
+
+    return jsonify({'error': 'This email id is not registered. Please contact admin.'}), HTTP_401_UNAUTHORIZED
